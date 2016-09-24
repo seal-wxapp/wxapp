d4965823bc6ced02f437fd3843aa51b728095092
kodo <524580860@qq.com>
2016-09-24 21:55:54 +0800

    add:新增 兼容 wxml wxss html 处理

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-const path  = require('path');
-const gulp  = require('gulp');
-const ugjs = require('gulp-uglify');
-const watch = require('gulp-watch');
-const named = require('vinyl-named');
-const postcss = require('gulp-postcss'); //postcss本身
-const webpackStream = require('webpack-stream');
-const watchPath = require('gulp-watch-path');
+var path  = require('path');
+var gulp  = require('gulp');
+var ugjs = require('gulp-uglify');
+var watch = require('gulp-watch');
+var named = require('vinyl-named');
+var postcss = require('gulp-postcss'); //postcss本身
+var webpackStream = require('webpack-stream');
+var watchPath = require('gulp-watch-path');
+var chalk = require('chalk');
+var rename = require('gulp-rename');
+var ifElse = require('gulp-if-else');
+var base64       = require('gulp-base64');
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss'); //提供像scss一样的语法
+var cssnano      = require('cssnano');  //更好用的css压缩!
+var sass         = require('gulp-sass');
 
-const rename = require('gulp-rename');
-// const ifElse = require('gulp-if-else');
-const base64       = require('gulp-base64');
-const autoprefixer = require('autoprefixer');
-const precss       = require('precss'); //提供像scss一样的语法
-const cssnano      = require('cssnano');  //更好用的css压缩!
-const sass         = require('gulp-sass');
-
+var isBuild = false;
 
 var webpackConfig = {
 	resolve: {
@@ -37,40 +38,37 @@ var webpackConfig = {
 	}
 };
 
-const processes = [
+var processes = [
 	autoprefixer({browsers: ['last 2 version', 'safari 5', 'opera 12.1', 'ios 6', 'android 4', '> 10%']}),
 	precss,
 	cssnano
 ];
 
-const src  = {
+var src  = {
 	fonts: './src/**/*.{eot,svg,ttf,woff}',
 	images: './src/**/*.{png,jpg,jpeg}',
 	js: './src/**/*.js',
 	sass: './src/**/*.scss',
+	wxss : './src/**/*.wxss',
+	wxml : './src/**/*.wxml',
 	json : './src/**/*.json',
-	views: './src/**/*.wxml'
+	views: './src/**/*.{html,wxml}'
 };
-const dist = {
-	css: './dist/',
-	fonts: './dist/',
-	images: './dist/',
-	js: './dist/',
-	views: './dist/'
-};
+var dist = './dist';
 gulp.task('dev',function() {
-	gulp.start('views','sass','images','fonts','js','json');
+	gulp.start('views','sass','wxss','images','fonts','js','json');
 });
 gulp.task('build',function() {
-	gulp.start('sass:build','js:build');
-	views();
-	images();
-	fonts();
-	json();
+	isBuild = true;
+	build(function () {
+		setTimeout(function() {
+			console.log(chalk.green('	Build success!'))
+		},0)
+	});
 });
 gulp.task('views', function () {
 	watch([src.views]).on('change',function() {
-		views()
+		views();
 	})
 });
 gulp.task('json', function () {
@@ -79,21 +77,19 @@ gulp.task('json', function () {
 	});
 });
 gulp.task('sass', function () {
-	watch([src.sass]).on('change', function () {
-		gulp.src(src.sass)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(postcss(processes))
-		.pipe(base64({
-			extensions: ['png', /\.jpg#datauri$/i],
-			maxImageSize: 10 * 1024 // bytes,
-		}))
-		.pipe(rename({
-			extname: ".wxss"
-		}))
-		.pipe(gulp.dest(dist.css));
+	watch([src.sass], function (event) {
+		var paths = watchPath(event, src.sass, dist);
+		compileSass(paths.srcPath)
 	});
 });
 
+gulp.task('wxss',function() {
+	watch([src.wxss], function (event) {
+		var paths = watchPath(event, src.wxss, dist);
+		compileWxss(paths.srcPath); // 编译 .wxss
+	});
+	
+});
 gulp.task('images', function () {
 	watch([src.images]).on('change',function() {
 		images();
@@ -105,35 +101,27 @@ gulp.task('fonts', function () {
 	})
 });
 gulp.task('js', function () {
-	
 	watch([src.js], function (event) {
-		var paths = watchPath(event, src.js, dist.js);
+		var paths = watchPath(event, src.js, dist);
 		compileJS(paths.srcPath);
 	})
-	
 });
+
+gulp.task('view:build',function() {
+	views()
+})
 gulp.task('sass:build', function () {
-	gulp.src(src.sass)
-	.pipe(sass().on('error', sass.logError))
-	.pipe(postcss(processes))
-	.pipe(base64({
-		extensions: ['png', /\.jpg#datauri$/i],
-		maxImageSize: 10 * 1024 // bytes,
-	}))
-	.pipe(postcss(processes))
-	.pipe(rename({
-		extname: ".wxss"
-	}))
-	.pipe(gulp.dest(dist.css));
+	compileSass(src.sass)
+});
+gulp.task('wxss:build', function () {
+	compileWxss(src.wxss)
 });
 gulp.task('js:build', function () {
-	gulp.src([src.js])
-		.pipe(ugjs())
-		.pipe(gulp.dest(dist.js));
+	compileJS([src.js]);
 });
-function compileJS(path) {
+function compileJS(path,cb) {
 	
-	return gulp.src(path)
+	gulp.src(path)
 	.pipe(named(function (file) {
 		var path = JSON.parse(JSON.stringify(file)).history[0];
 		var target = path.split('src/')[1];
@@ -143,22 +131,59 @@ function compileJS(path) {
 	.on('error',function(err) {
 		this.end()
 	})
-	.pipe(gulp.dest(dist.js))
+	.pipe(ifElse(isBuild === true,ugjs))
+	.pipe(gulp.dest(dist))
+	cb && cb();
+}
+function compileWxss(src) {
+	gulp.src(src)
+	.pipe(base64({
+		extensions: ['png', /\.jpg#datauri$/i],
+		maxImageSize: 10 * 1024 // bytes,
+	}))
+	.pipe(postcss(processes))
+	.pipe(gulp.dest(dist))
+}
+function compileSass(src) {
+	gulp.src(src)
+	.pipe(sass().on('error', sass.logError))
+	.pipe(base64({
+		extensions: ['png', /\.jpg#datauri$/i],
+		maxImageSize: 10 * 1024 // bytes,
+	}))
+	.pipe(ifElse(isBuild === true,function() {
+		return postcss(processes);
+	}))
+	.pipe(rename({
+		extname: ".wxss"
+	}))
+	.pipe(gulp.dest(dist));
 }
 function fonts() {
 	
 	gulp.src(src.fonts)
-	.pipe(gulp.dest(dist.fonts));
+	.pipe(gulp.dest(dist));
 }
 function images() {
 	gulp.src(src.images)
-	.pipe(gulp.dest(dist.images));
+	.pipe(gulp.dest(dist));
 }
 function views() {
 	gulp.src(src.views)
-	.pipe(gulp.dest(dist.views));
+	.pipe(rename({
+		extname: ".wxml"
+	}))
+	.pipe(gulp.dest(dist));
 }
 function json() {
 	gulp.src(src.json)
-		.pipe(gulp.dest(dist.js));
+	.pipe(gulp.dest(dist))
+}
+function build(cb) {
+	gulp.start('sass:build','wxss:build','js:build');
+	views();
+	images();
+	fonts();
+	json();
+	cb && cb()
 }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,7 @@
 //index.js
 //获取应用实例
+var util = require('../../utils/util');
+console.log(util);
 var app = getApp()
 Page({
   data: {
@@ -16,6 +18,9 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
+	  pro('lalal').then(res => {
+	  	console.log(res);
+	  });
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
@@ -24,3 +29,11 @@ Page({
     })
   }
 })
+
+function pro(msg) {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(msg)
+		},1000)
+	})
+}
